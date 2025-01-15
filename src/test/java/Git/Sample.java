f06240e75ac2c78e30ca4a57c5ee6c06e2cee58a
Prasannakumar553 <prasanghata.kumar@gmail.com>
2025-01-15 13:06:14 +0530

    First Commit

---
+++ src/test/java/Git/Sample.java
@@ -0,0 +1,23 @@
+package Git;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		Sample obj = new Sample();
+		
+		obj.greet();
+		obj.Wish();
+	     
+		
+	}
+	
+	private void greet() {
+		System.out.println("Good Morning");
+		
+	}
+	private void Wish() {
+System.out.println("Good Afternoon");
+	}
+
+	
+}
